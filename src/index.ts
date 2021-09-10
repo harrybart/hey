38c4d00347e3fd0c348628d24396c2b261a85e71
Harry Bart <animationsbart@gmail.com>
2021-09-10 15:09:51 +0000

    blah

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 const router = express.Router();
 router.get("/", (_req, res) => {
-  readFile(join(__dirname, "/client/index.html"), (_err, data) => {
+  readFile(join(__dirname, "../client/index.html"), (_err, data) => {
     if (data) {
       res.write(data);
     }
@@ -18,7 +18,9 @@ router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (_req, res) => res.sendFile(join(__dirname, "../index.html")));
+app.use("/", (_req, res) =>
+  res.sendFile(join(__dirname, "../client/index.html"))
+);
 
 module.exports = app;
 module.exports.handler = serverless(app);
