6971042666e84d09ab2e5369dbdee99b2ebf0a3b
Harry Bart <animationsbart@gmail.com>
2021-09-10 15:07:45 +0000

    blah

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,24 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
 import express from "express";
 import serverless from "serverless-http";
 import { join } from "path";
+import { readFile } from "fs";
 
 const app = express();
 
-// const messages = [];
-
-app.use(express.static("client"));
-
 const router = express.Router();
 router.get("/", (_req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js!</h1>");
+  readFile(join(__dirname, "/client/index.html"), (_err, data) => {
+    if (data) {
+      res.write(data);
+    }
+  });
   res.end();
 });
-router.get("/chats", (_req, res) => res.redirect("/chats.html"));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
-app.use("/.netlify/functions/server", router);
-app.use("/", (_req, res) =>
-  res.sendFile(join(__dirname, "../client/index.html"))
-);
-
-const httpServer = createServer(app);
-
-const webSocket = new Server(httpServer, {
-  serveClient: true,
-});
-
-webSocket.on("connection", (socket) => {
-  console.log("Websocket connected", socket.id);
-});
-
-// httpServer.listen(3000, () => {
-//   console.log("Webserver started");
-// });
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (_req, res) => res.sendFile(join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
